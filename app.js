1e1e41b7bffa694b2946a20cbf884403ebc9a79e
yourtechie <brookeyajayi55@gmail.com>
2026-06-02 20:46:43 +0100

    Made a change on App.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀January Cohort CI/CD Deployment Successful!');
+  res.send('🚀January Cohort CI/CD Deployment Successful! We just added a new feature!!!');
 });
 
 app.listen(PORT, () => {
